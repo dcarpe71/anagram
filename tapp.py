5ed98057f2cd18074e7b18fd9b3fec38ac452de7
dcarpe71 <dcarpe71@gmail.com>
2017-09-28 23:42:26 -0400

    Removed the permutations array and sorted the search word iterating on the dictionary file and for every word found that was the same length as the sorted word would be sorted and compared and added to a result list.

--- tapp.py
+++ tapp.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 from flask import Flask, request
 from flask_restful import Resource, Api
-from json import dumps
 from collections import defaultdict
 from itertools import permutations
+import json
 import sys
 
 reload(sys)
@@ -12,14 +12,29 @@ sys.setdefaultencoding('utf8')
 app = Flask(__name__)
 api = Api(app)
 
-def print_anagram(word_array):
+def get_anagrams(word, result_list):
+    
+    # open the file
     searchfile = open("/home/dcv/words", "r")
+
+    # sort the word to test
+    word_sorted = sorted(word)
+    word_len = len(word_sorted)
+
+    # iterate through the file and test
     for line in searchfile:
-        print line
-        for val in word_array:
-            if val in line:
-            #if val == line:
-                print val
+    
+        # strip newline
+        l = line[0:len(line)-1]
+
+        # check sizes of words, ignore if they differ
+        if word_len == len(l):
+            s = sorted(l)
+            # compare strings
+            if word_sorted == s:
+                result_list.append(l)
+                #print "found a match: " + l
+
     searchfile.close()
 
 class List_dict(Resource):
@@ -31,14 +46,13 @@ class List_dict(Resource):
 	return data
 
 class Anagram_search(Resource):
-    def get(self, source_word):
-        from itertools import permutations
-        import enchant
-        d = enchant.Dict('en_US')
-        anagram_array = [''.join(p) for p in permutations(source_word, len(source_word)) if d.check(''.join(p))== True]
-        print_anagram(anagram_array)
-        return anagram_array
-       
+      def get(self, source_word):
+        res_list = []
+        get_anagrams(source_word, res_list)
+	r = json.dumps(res_list)	
+	print r
+        return r
+ 
 api.add_resource(Anagram_search, '/anagram/<string:source_word>')
 api.add_resource(List_dict, '/dictionary')
 
