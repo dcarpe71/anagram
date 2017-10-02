d7ee983420a55189593100fe120e054fbe05468c
dcarpe71 <dcarpe71@gmail.com>
2017-10-02 17:45:42 -0400

    fix copy words

--- deploy_scr.sh
+++ deploy_scr.sh
@@ -2,5 +2,5 @@ git clone https://github.com/dcarpe71/anagram.git
 cd anagram
 cp tapp.py ttest.sh /usr/local/bin
 cp web/index.html /var/www/html
-cp /usr/share/dict/american-english /var/www/html/words
 cp web/anagram_cgi.py /var/www/html/cgi-bin
+cp /usr/share/dict/american-english /var/www/html/cgi-bin/words
