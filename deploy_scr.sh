c93422e0e4697debf183f544ae883271a9e6122f
dcarpe71 <dcarpe71@gmail.com>
2017-10-02 17:24:26 -0400

    update deploy script to include dictionary copy

--- deploy_scr.sh
+++ deploy_scr.sh
@@ -2,5 +2,6 @@ git clone https://github.com/dcarpe71/anagram.git
 cd anagram
 cp tapp.py ttest.sh /usr/local/bin
 cp web/index.html /var/www/html
+cp /usr/share/dict/american-english /var/www/html/words
 mkdir /var/www/html/cgi-bin
 cp web/anagram_cgi.py /var/www/html/cgi-bin
