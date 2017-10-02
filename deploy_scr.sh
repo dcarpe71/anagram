3ad117d3994212698bd257753e3245cb67576d27
dcarpe71 <dcarpe71@gmail.com>
2017-10-02 17:36:23 -0400

    stop creatgin cgi-bin dir

--- deploy_scr.sh
+++ deploy_scr.sh
@@ -3,5 +3,4 @@ cd anagram
 cp tapp.py ttest.sh /usr/local/bin
 cp web/index.html /var/www/html
 cp /usr/share/dict/american-english /var/www/html/words
-mkdir /var/www/html/cgi-bin
 cp web/anagram_cgi.py /var/www/html/cgi-bin
