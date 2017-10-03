13da739fce2653ed2044283a29a6a36446dd2ee7
dcarpe71 <dcarpe71@gmail.com>
2017-10-03 14:32:22 -0400

    Commented out git clone from deploy

---
+++ anagram/deploy_scr.sh
@@ -0,0 +1,6 @@
+### git clone https://github.com/dcarpe71/anagram.git ### Run Manually
+cd anagram
+cp tapp.py ttest.sh /usr/local/bin
+cp web/index.html /var/www/html
+cp web/anagram_cgi.py /var/www/html/cgi-bin
+cp /usr/share/dict/american-english /var/www/html/cgi-bin/words
